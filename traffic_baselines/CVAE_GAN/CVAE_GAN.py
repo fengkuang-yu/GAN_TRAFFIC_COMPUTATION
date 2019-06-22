4058d658732a2c14a2c063607ece3afc3c186d8d
fengkuangshine <1316305178@qq.com>
2019-06-22 12:14:56 +0800

    create

--- traffic_baselines/CVAE_GAN/CVAE_GAN.py
+++ traffic_baselines/CVAE_GAN/CVAE_GAN.py
@@ -15,9 +15,8 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-import keras
-from keras.engine.topology import Layer
-from keras.layers import Input, Dense, Lambda, LeakyReLU, Dropout, Flatten, Reshape
+from keras.layers import Input, Dense, Lambda, LeakyReLU
+from keras.layers import Flatten, Reshape, Concatenate
 from keras.layers import BatchNormalization, Activation, ZeroPadding2D
 from keras.layers.convolutional import UpSampling2D, Conv2D
 
@@ -26,7 +25,6 @@ from keras import backend as K
 from keras.optimizers import RMSprop
 from keras.datasets import mnist
 from keras.layers.merge import _Merge
-from functools import partial
 from tqdm import tqdm
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
@@ -39,6 +37,7 @@ class RandomWeightedAverage(_Merge):
         alpha = K.random_uniform((100, 1, 1, 1))
         return (alpha * inputs[0]) + ((1 - alpha) * inputs[1])
 
+
 class VAE_GAN:
     def __init__(self):
         self.batch_size = 100
@@ -49,10 +48,17 @@ class VAE_GAN:
         self.img_shape = (self.rows, self.cols, self.channels)
         self.latent_dim = 100
         self.intermediate_dim = 256
+        
         self.gamma = 0.1
+        self.penalty = 10
+        self.lambda1 = 3
+        self.lambda2 = 1
+        self.lambda3 = 1e-3
+        self.lambda4 = 1e-3
+        
         self.epochs = 5000
         self.n_critic = 5
-        self.num_attrs = 10
+        self.n_attrs = 10
         self.optimizer = RMSprop()
         
         # 构建编码器，生成器和判别器
@@ -64,72 +70,110 @@ class VAE_GAN:
         self.encoder_trainer = None
         self.decoder_trainer = None
         self.critic_trainer = None
+        self.classifier_trainer = None
         
         self.build_model()
     
     def build_model(self):
-        real_x = Input(shape=self.img_shape, name='real_image')
-        z_mean, z_log_var = self.encoder(real_x)
+        # Algorithm
+        x_real = Input(shape=self.img_shape, name='real_image')
+        c_input = Input(shape=(self.n_attrs,), name='condition_input')
         
+        z_mean, z_log_var = self.encoder([x_real, c_input])
         z = Lambda(self.sampling, output_shape=(self.latent_dim,), name='re-sampling_layer')([z_mean, z_log_var])
-        # kl_loss = Lambda()
-        
-        reconstruct_x = self.decoder(z)
-        reconstruct_x_valid, reconstruct_llike = self.critic(reconstruct_x)
-        real_x_valid, real_x_valid_llike = self.critic(real_x)
-        
-        interpolated_img = RandomWeightedAverage()([real_x, reconstruct_x])
-        # Determine validity of weighted sample
-        validity_interpolated, validity_interpolated_llike = self.critic(interpolated_img)
-        partial_gp_loss_rec = partial(self.gradient_penalty_loss, averaged_samples=interpolated_img)
-        partial_gp_loss_rec.__name__ = 'gradient_penalty'  # Keras requires function names
+        kl_loss = - 0.5 * K.sum(1 + z_log_var - K.square(z_mean) - K.exp(z_log_var), axis=-1)
         
         z_p = Input(shape=(self.latent_dim,), name='random_distribution')
-        gen_x = self.decoder(z_p)
-        random_gen_x_valid, random_gen_x_valid_llike = self.critic(gen_x)
+        x_rec = self.decoder([z, c_input])
+        inter_img = RandomWeightedAverage()([x_real, x_rec])
         
-        ### 构建VAE_GAN的编码部分 ###
-        self.encoder.trainable = True
+        x_gen = self.decoder([z_p, c_input])
+        
+        inter_valid, inter_llike = self.critic(inter_img)
+        rec_valid, rec_d_llike = self.critic(x_rec)
+        real_valid, real_d_llike = self.critic(x_real)
+        gen_valid, gen_d_llike = self.critic(x_gen)
+        
+        rec_class, rec_c_llike = self.classifier(x_rec)
+        real_class, real_c_llike = self.classifier(x_real)
+        gen_class, gen_c_llike = self.classifier(x_gen)
+        
+        val_loss = Lambda(self.valid_loss, output_shape=(1,),
+                          name='d_loss')([rec_valid, real_valid, gen_valid, inter_img, inter_valid])
+        gen_loss = Lambda(self.generative_loss, output_shape=self.img_shape,
+                          name='generative_loss')([x_real, x_rec, real_d_llike, rec_d_llike, real_c_llike, rec_c_llike])
+        d_llike_loss = Lambda(self.mean_gaussian_negative_log_likelihood, output_shape=(1,),
+                              name='mean_gaussian_negative_log_likelihood_D')([real_d_llike, gen_d_llike])
+        c_llike_loss = Lambda(self.mean_gaussian_negative_log_likelihood, output_shape=(1,),
+                              name='mean_gaussian_negative_log_likelihood_C')([real_c_llike, gen_c_llike])
+        c_class_loss = Lambda(self.classifier_loss, output_shape=(self.n_attrs,),
+                              name='classifier_loss')([c_input, real_class])
+        
+        ### build classifier ###
+        self.encoder.trainable = False
         self.decoder.trainable = False
         self.critic.trainable = False
-        self.encoder_trainer = Model(inputs=real_x, outputs=[reconstruct_x, reconstruct_llike], name='encoder')
-        kl_loss = - 0.5 * K.sum(1 + z_log_var - K.square(z_mean) - K.exp(z_log_var), axis=-1)
-        reconstruct_loss = self.mean_gaussian_negative_log_likelihood(real_x_valid_llike, reconstruct_llike)
-        encoder_loss = K.mean(kl_loss + reconstruct_loss)
-        self.encoder_trainer.add_loss(encoder_loss)
-        self.encoder_trainer.compile(optimizer=self.optimizer)
-        # self.encoder.summary()
+        self.classifier.trainable = True
+        self.classifier_trainer = Model([x_real, c_input], [c_class_loss], name='classifier')
+        self.classifier_trainer.add_loss(c_class_loss)
+        self.classifier_trainer.compile(optimizer=self.optimizer)
+        self.classifier_trainer.summary()
         
-        ### 构建VAE_GAN的生成部分 ###
-        self.encoder.trainable = False
-        self.decoder.trainable = True
-        self.critic.trainable = False
-        self.decoder_trainer = Model(inputs=[real_x],
-                                     outputs=[reconstruct_llike, reconstruct_x_valid],
-                                     name='decoder')
-        decoder_valid_loss = self.wasserstein_loss(-np.ones((self.batch_size, 1)), reconstruct_x_valid)
-        decoder_loss = self.gamma * reconstruct_loss + 1000 * decoder_valid_loss
-        self.decoder_trainer.add_loss(decoder_loss)
-        self.decoder_trainer.compile(optimizer=self.optimizer)
-        # self.decoder_trainer.summary()
-        
-        ### 构建GAN的判别部分
+        ### build discriminator ###
         self.encoder.trainable = False
         self.decoder.trainable = False
         self.critic.trainable = True
-        self.critic_trainer = Model(inputs=[real_x, z_p],
-                                    outputs=[validity_interpolated,
-                                             random_gen_x_valid,
-                                             reconstruct_x_valid,
-                                             real_x_valid, ],
-                                    name='critic')
-        self.critic_trainer.compile(loss=[partial_gp_loss_rec,
-                                          self.wasserstein_loss,
-                                          self.wasserstein_loss,
-                                          self.wasserstein_loss, ],
-                                    optimizer=self.optimizer,
-                                    loss_weights=[10, 1, 1, 2])
+        self.classifier.trainable = False
+        self.critic_trainer = Model([x_real, c_input, z_p], [val_loss], name='discriminator')
+        self.critic_trainer.add_loss(val_loss)
+        self.critic_trainer.compile(optimizer=self.optimizer)
         # self.critic_trainer.summary()
+        
+        ### build decoder ###
+        self.encoder.trainable = False
+        self.decoder.trainable = True
+        self.critic.trainable = False
+        self.classifier.trainable = False
+        self.decoder_trainer = Model([x_real, c_input, z_p],
+                                     [x_rec, gen_loss, d_llike_loss, c_llike_loss], name='decoder')
+        self.decoder_trainer.add_loss(
+            self.lambda2 * gen_loss + self.lambda3 * d_llike_loss + self.lambda4 * c_llike_loss)
+        self.decoder_trainer.compile(optimizer=self.optimizer)
+        self.decoder_trainer.summary()
+        
+        ### bulid encoder ###
+        self.encoder.trainable = True
+        self.decoder.trainable = False
+        self.critic.trainable = False
+        self.classifier.trainable = False
+        self.encoder_trainer = Model([x_real, c_input, z_p],
+                                     [x_rec, gen_loss], name='encoder')
+        self.encoder_trainer.add_loss(self.lambda1 * kl_loss + self.lambda2 * gen_loss)
+        self.encoder_trainer.compile(optimizer=self.optimizer)
+        self.encoder.summary()
+    
+    def valid_loss(self, args):
+        rec_valid, real_valid, gen_valid, inter_imgs, inter_valid = args
+        valid = -np.ones((self.batch_size, 1))
+        fake = np.ones((self.batch_size, 1))
+        
+        rec_valid_loss = self.wasserstein_loss(rec_valid, fake)
+        real_valid_loss = self.wasserstein_loss(real_valid, valid)
+        gen_valid_loss = self.wasserstein_loss(gen_valid, fake)
+        penalty_loss = self.gradient_penalty_loss(None, inter_valid, inter_imgs)
+        
+        return rec_valid_loss + 2 * real_valid_loss + gen_valid_loss + self.penalty * penalty_loss
+    
+    def generative_loss(self, args):
+        x_real, x_rec, real_d_llike, rec_d_llike, real_c_llike, rec_c_llike = args
+        x_loss = K.mean(K.square(x_real - x_rec))
+        d_loss = K.mean(K.square(real_d_llike - rec_d_llike))
+        c_loss = K.mean(K.square(real_c_llike - rec_c_llike))
+        return x_loss + d_loss + c_loss
+    
+    def classifier_loss(self, args):
+        c_input, real_class = args
+        return K.mean(K.categorical_crossentropy(c_input, real_class))
     
     def build_encoder(self):
         img = Input(shape=self.img_shape)
@@ -147,16 +191,18 @@ class VAE_GAN:
         conv3_output = LeakyReLU(alpha=0.2)(conv3)
         
         fc1 = Flatten()(conv3_output)
+        c_input = Input(shape=(self.n_attrs,))
+        fc_output = Concatenate(axis=-1)([fc1, c_input])
         
         # 算p(Z|X)的均值和方差
-        z_mean = Dense(self.latent_dim)(fc1)
-        z_log_var = Dense(self.latent_dim)(fc1)
-        return Model(img, [z_mean, z_log_var])
+        z_mean = Dense(self.latent_dim)(fc_output)
+        z_log_var = Dense(self.latent_dim)(fc_output)
+        return Model([img, c_input], [z_mean, z_log_var])
     
     def build_decoder(self):
         model = Sequential()
         
-        model.add(Dense(128 * 7 * 7, activation="relu", input_dim=self.latent_dim))
+        model.add(Dense(128 * 7 * 7, activation="relu", input_dim=self.latent_dim+self.n_attrs))
         model.add(Reshape((7, 7, 128)))
         model.add(UpSampling2D())
         model.add(Conv2D(128, kernel_size=4, padding="same"))
@@ -169,10 +215,13 @@ class VAE_GAN:
         model.add(Conv2D(self.channels, kernel_size=4, padding="same"))
         model.add(Activation("sigmoid"))
         
-        noise = Input(shape=(self.latent_dim,))
+        z_noise = Input(shape=(self.latent_dim,))
+        c_input = Input(shape=(self.n_attrs,))
+        noise = Concatenate()([z_noise, c_input])
+        
         img = model(noise)
         
-        return Model(noise, img)
+        return Model([z_noise, c_input], img)
     
     def build_critic(self):
         
@@ -197,31 +246,29 @@ class VAE_GAN:
     
     def build_classifier(self):
         img = Input(shape=self.img_shape)
-
+        
         conv1 = Conv2D(16, kernel_size=3, strides=2, input_shape=self.img_shape, padding="same")(img)
         conv1 = BatchNormalization(momentum=0.8)(conv1)
         conv1 = LeakyReLU(alpha=0.2)(conv1)
-
+        
         conv2 = Conv2D(32, kernel_size=3, strides=2, padding="same")(conv1)
         conv2 = ZeroPadding2D(padding=((0, 1), (0, 1)))(conv2)  # 判断是否需要填充
         conv2 = BatchNormalization(momentum=0.8)(conv2)
         conv2 = LeakyReLU(alpha=0.2)(conv2)
-
+        
         conv3 = Conv2D(64, kernel_size=3, strides=2, padding="same")(conv2)
         conv3 = BatchNormalization(momentum=0.8)(conv3)
         conv3_output = LeakyReLU(alpha=0.2)(conv3)
-
+        
         fc1 = Flatten()(conv3_output)
-
-        f = Flatten()(fc1)
-        x = Dense(1024)(f)
+        
+        x = Dense(1024)(fc1)
         x = Activation('relu')(x)
-    
-        x = Dense(self.num_attrs)(x)
-        x = Activation('softmax')(x)
-    
-        return Model(img, [x, f])
-        pass
+        
+        x = Dense(self.n_attrs)(x)
+        x_out = Activation('softmax')(x)
+        
+        return Model(img, [x_out, fc1])
     
     def train(self):
         # 加载MNIST数据集
@@ -270,19 +317,12 @@ class VAE_GAN:
     def wasserstein_loss(self, y_true, y_pred):
         return K.mean(y_true * y_pred)
     
-    def mean_gaussian_negative_log_likelihood(self, y_true, y_pred):
+    def mean_gaussian_negative_log_likelihood(self, args):
+        y_true, y_pred = args
         # 实际上就是mse
         nll = 0.5 * np.log(2 * np.pi) + 0.5 * K.square(y_pred - y_true)
         axis = tuple(range(1, len(K.int_shape(y_true))))
         return K.mean(K.sum(nll, axis=axis), axis=-1)
-    
-    def zero_loss(self, y_true, y_pred):
-        """
-        args:
-            y_true():
-            y_pred():
-        """
-        return K.zeros_like(y_true)
     
     def sampling(self, args):
         z_mean, z_log_var = args
